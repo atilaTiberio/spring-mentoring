3da61836b807b7103558a0803c29653c561841fb
hiturbe <Igarcia@2020>
2020-02-17 12:33:57 -0600

    Rest controller added

---
+++ springapp/src/main/java/mx/internetbrands/spring/beana/controller/IndexRestController.java
@@ -0,0 +1,8 @@
+package mx.internetbrands.spring.beana.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/rest")
+public class IndexRestController {
+
+}
